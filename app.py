34d2cf98f96f9b05e5ab273d1d98688865521682
lukemayo42 <123115913+lukemayo42@users.noreply.github.com>
2024-12-01 16:09:02 -0500

    login form

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template, url_for, request, redirect, session
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
-def index():
-    return render_template("/index.html")
+def login():
+    if request.method == 'POST':
+        return render_template('index.html')
+    else:
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
