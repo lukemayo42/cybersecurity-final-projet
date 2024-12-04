95327f29b6628215665cad5f06e2a02bf04e2402
lukemayo42 <123115913+lukemayo42@users.noreply.github.com>
2024-12-04 15:13:01 -0500

    register button

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 def login():
     if request.method == 'POST':
         print("helloe")
-        if 'submit' in request.form and request.form['submit'] == 'Submit':
-            print("submit")
-            return render_template('index.html')
-        else:
-            redirect('/register')
+        # login attempt
+        if 'submit' in request.form: 
+            # check to see if the username is in the database
+            # if the user is in the database, check if the password is correct
 
-        #check to see what button they pressed, submit or register new user
-        #if submit is pressed
+                # if username and password are correct
+                    # go to index.html
+                # else
+                    # return to login.html with message saying that password is incorrect
+            #else
+                #return to login.html saying that username is not in the database:
+            print("submit")
             # check to see if the username is in the database
             # if the user is in the database, check if the password is correct
 
@@ -23,14 +27,31 @@ def login():
                     # return to login.html with message saying that password is incorrect
             #else
                 #return to login.html saying that username is not in the database
-        return render_template('index.html')
+            return render_template('index.html')
+        # register new user
+        elif 'register1' in request.form:
+            return render_template('login.html')
+        
+        else:
+            return redirect('/register')
+            
     else:
         return render_template("login.html")
     
 @app.route('/register', methods = ['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        return render_template("login.html")
+        print("redirecting")
+        return redirect("/")
+        # get the form data
+        
+        # check that it meets requirements
+            # try to add to the database
+            # catch exception, display error message, return back to 
+        
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
